9848a23b957c2421938407ec636e13cfa784b93c
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-05-16 13:08:48 -0700

    Update configuration.py
    added table name

--- configuration.py
+++ configuration.py
@@ -2,5 +2,6 @@ db_host = "localhost"  # IP address of the MySQL database server
 db_user = "YOUR_USERNAME"  # User name of the database server
 db_password = "YOUR_PASSWORD" # Password for the database user
 db_name = "PMonData"  #database to be accessed
-db_port = 1886  #port used by db
+db_port = 1886  #port used by database
 dir_path = '/some/path/to/directory'  #path to directory where csv files are located
+table_name = "IOBoardx" #can change database table to any name, for example IOBoard1
