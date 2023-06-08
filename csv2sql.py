d75bcdea9d05742d3c0f7c979c3905564c5957b9
MichaelChestnut <72172361+MichaelChestnut@users.noreply.github.com>
2023-06-08 15:59:11 -0700

    Update csv2sql.py

--- csv2sql.py
+++ csv2sql.py
@@ -11,16 +11,11 @@ import pytz
 
 logging.basicConfig(filename='/home/supervisor/CSVtoSQL/csv2sql.log', level=logging.DEBUG)
 
-# IP address of the MySQL database server
-DB_HOST = config.db_host
-# User name of the database server
-DB_USER = config.db_user
-# Password for the database user
-DB_PASSWORD = config.db_password
-#database to be accessed
-DB_NAME = config.db_name
-#port used by db
-DB_PORT = config.db_port
+DB_HOST = config.db_host # IP address of the MySQL database server
+DB_USER = config.db_user # User name of the database server
+DB_PASSWORD = config.db_password # Password for the database user
+DB_NAME = config.db_name # Database to be accessed
+DB_PORT = config.db_port # Port used by db
 
 # Establish path to directory containing all other data directories
 Parent_Dir_Path = config.parent_dir_path
@@ -30,7 +25,7 @@ Directories = os.listdir(Parent_Dir_Path)
 
 
 # Define function
-def get_last_csv_line(File_Path):
+def get_last_csv_line(file_path):
     # empty string to later convert list into a string in order to use find
     tempstring = ''
     with open(File_Path, 'r') as file:
@@ -50,23 +45,90 @@ def get_last_csv_line(File_Path):
         else:
             logging.debug(f"{File_Path} is empty or all data is corrupt")
             return None
+        
+def create_database(cursor, db_name):
+    #create database if it does not exist
+    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
 
+    #check if database was created
+    if cursor.rowcount == -1:
+       logging.info("Database already exists or an error occurred.")
+    else:
+       logging.info("Database created successfully.")
+    
+def create_table(cursor, table_name):
+    #create table if it does not exist
+    create_table_query = f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
+           id INT NOT NULL AUTO_INCREMENT,
+           DateAndTime DATETIME(6) UNIQUE NOT NULL,
+           LoadName CHAR(30) NOT NULL,
+           ShuntVoltage FLOAT,
+           LoadVoltage FLOAT,
+           Current FLOAT,
+           Power FLOAT,
+           PRIMARY KEY (id)
+           );"""
+    
+    # To execute the SQL query
+    cursor.execute(create_table_query)
 
+    #check if table was created
+    if cursor.rowcount == -1:
+      logging.info("Table already exists or an error occurred.")
+    else:
+       logging.info("Table created successfully.")
+
+    
+ def check_file_in_db(cursor, table_name, file_path):
+     #get the last line of the current file
+     last_line = get_last_csv_line(File_Path)
+     #Parse Last_Line tuple into individual variables and convert variables into correct data types
+     DateAndTime, LoadName, ShuntVoltage, LoadVoltage, Current, Power = last_line
+     DandT = datetime.datetime.fromisoformat(DateAndTime)
+     if DandT.tzinfo != pytz.UTC:
+        DandT = DandT.astimezone(pytz.UTC) #convert timestamp to UTC if it is not already
+     DateAndTime = DandT.strftime('%Y-%m-%d %H:%M:%S.%f')
+     DateAndTime = datetime.datetime.strptime(DateAndTime, "%Y-%m-%d %H:%M:%S.%f")
+     ShuntVoltage = float(ShuntVoltage)
+     LoadVoltage = float(LoadVoltage)
+     Current = float(Current)
+     Power = float(Power)
+
+     #The following is for debugging to ensure values and datatypes are correct before querying
+     #DnT = type(DateAndTime)
+     #LN = type(LoadName)
+     #SV = type(ShuntVoltage)
+     #LV = type(LoadVoltage)
+     #C = type(Current)
+     #P = type(Power)
+     #print(f"DateAndTime is {DateAndTime} of type {DnT}")
+     #print(f"LoadName is {LoadName} of type {LN}")
+     #print(f"ShuntVoltage is {ShuntVoltage} of type {SV}")
+     #print(f"LoadVoltage is {LoadVoltage} of type {LV}")
+     #print(f"Current is {Current} of type {C}")
+     #print(f"Power is {Power} of type {P}")
+
+     # Execute query to check if line already exists in database
+     query = "SELECT * FROM {0} WHERE DateAndTime = '{1}' AND ShuntVoltage LIKE {2} AND LoadVoltage LIKE {3} AND Current LIKE {4} AND Power LIKE {5};"
+     query = query.format(TABLE_NAME, DateAndTime, ShuntVoltage, LoadVoltage, Current, Power)
+     cursor.execute(query)
+    
+     # Fetch the result of the query
+     row = cursor.fetchone()
+     return row
+
+    
 # Create a connection object
 conn = mariadb.connect(user=DB_USER,
                        password=DB_PASSWORD,
                        host=DB_HOST,
                        port=DB_PORT)
 
+# create a cusor object
 cursor = conn.cursor()
 
-#create database if it does not exist
-cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DB_NAME}")
-
-#check if database was created
-if cursor.rowcount == -1:
-    logging.info("Database already exists or an error occurred.")
-
+create_database(cursor, DB_NAME) # Parameters are (cursor, database name)
+    
 #close the cursor
 cursor.close()
 
@@ -90,24 +152,7 @@ for Directory in Directories:
   #create a new cursor object
   cursor = conn.cursor()
 
-  #create table if it does not exist
-  create_table_query = f"""CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
-           id INT NOT NULL AUTO_INCREMENT,
-           DateAndTime DATETIME(6) UNIQUE NOT NULL,
-           LoadName CHAR(30) NOT NULL,
-           ShuntVoltage FLOAT,
-           LoadVoltage FLOAT,
-           Current FLOAT,
-           Power FLOAT,
-           PRIMARY KEY (id)
-           );"""
-
-  # To execute the SQL query
-  cursor.execute(create_table_query)
-
-  #check if table was created
-  if cursor.rowcount == -1:
-      logging.info("Table already exists or an error occurred.")
+  create_table(cursor, TABLE_NAME) # Parameters are (cursor, table name)
 
   # Get the current time
   Current_Time = datetime.datetime.now()
